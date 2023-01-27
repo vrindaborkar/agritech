f180249b124d27c8f8ba7abc2ab5d5778d33f97d
roshnirajan1106 <roshnirajan8888@gmail.com>
2023-01-27 20:05:17 +0530

    Done with registeration

--- client/src/Routes/Register.js
+++ client/src/Routes/Register.js
@@ -201,83 +201,83 @@ export default function Register({ t, languages }) {
     window.confirmationResult
       .confirm(code)
       .then((result) => {
-        // User signed in successfully.
-        // const user = result.user;
-        // console.log(JSON.stringify(user));
-        // alert("User is verified");
+        //User signed in successfully.
+        const user = result.user;
+        console.log(JSON.stringify(user));
+        alert("User is verified");
         //window.location.href = "/newpassword";
         //handleSubmit()
-        console.log("inside handleSubmit")
-        seterror("");
+        // console.log("inside handleSubmit")
+        // seterror("");
 
-        const { phone, firstname, lastname, farmertype, type } = data;
+        // const { phone, firstname, lastname, farmertype, type } = data;
 
-        if (!phone.match("[0-9]{10}")) {
-          seterror("Please provide valid phone number");
-        } else if (firstname.length === 0 && lastname.length === 0) {
-          seterror("Please provide valid first and last name");
-        } else if (type.length === 0) {
-          seterror("Please select type");
-        } else if (type === "farmer" && farmertype.length === 0) {
-          seterror("select producer type");
-        } else if (type === "farmer" && tags.length === 0) {
-          seterror("select atleast one commodity and press enter");
-        } else {
-          seterror("no error");
-          setLoading(true);
-          AuthService.register(
-            data.phone,
-            data.password,
-            data.firstname,
-            data.lastname,
-            data.type,
-            data.farmertype,
-            data.address,
-            tags
-          ).then(
-            () => {
-              toast.success("Registration successful!", {
-                position: "top-center",
-                autoClose: 3000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-              });
-              setTimeout(() => {
-                navigate("/registeration-successfull");
-                window.location.reload();
-              }, 1000);
-            },
-            (error) => {
-              toast.warn("User Already Exists", {
-                position: "top-center",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "light",
-              });
-              setData({
-                phone: "",
-                password: "",
-                firstname: "",
-                lastname: "",
-                type: "",
-                farmertype: "",
-                address: "",
-              });
-              setTimeout(() => {
-                navigate("/login");
-                window.location.reload();
-              }, 1000);
-            }
-          );
-        }
+        // if (!phone.match("[0-9]{10}")) {
+        //   seterror("Please provide valid phone number");
+        // } else if (firstname.length === 0 && lastname.length === 0) {
+        //   seterror("Please provide valid first and last name");
+        // } else if (type.length === 0) {
+        //   seterror("Please select type");
+        // } else if (type === "farmer" && farmertype.length === 0) {
+        //   seterror("select producer type");
+        // } else if (type === "farmer" && tags.length === 0) {
+        //   seterror("select atleast one commodity and press enter");
+        // } else {
+        //   seterror("no error");
+        //   setLoading(true);
+        //   AuthService.register(
+        //     data.phone,
+        //     data.password,
+        //     data.firstname,
+        //     data.lastname,
+        //     data.type,
+        //     data.farmertype,
+        //     data.address,
+        //     tags
+        //   ).then(
+        //     () => {
+        //       toast.success("Registration successful!", {
+        //         position: "top-center",
+        //         autoClose: 3000,
+        //         hideProgressBar: false,
+        //         closeOnClick: true,
+        //         pauseOnHover: true,
+        //         draggable: true,
+        //         progress: undefined,
+        //         theme: "light",
+        //       });
+        //       setTimeout(() => {
+        //         navigate("/registeration-successfull");
+        //         window.location.reload();
+        //       }, 1000);
+        //     },
+        //     (error) => {
+        //       toast.warn("User Already Exists", {
+        //         position: "top-center",
+        //         autoClose: 5000,
+        //         hideProgressBar: false,
+        //         closeOnClick: true,
+        //         pauseOnHover: true,
+        //         draggable: true,
+        //         progress: undefined,
+        //         theme: "light",
+        //       });
+        //       setData({
+        //         phone: "",
+        //         password: "",
+        //         firstname: "",
+        //         lastname: "",
+        //         type: "",
+        //         farmertype: "",
+        //         address: "",
+        //       });
+        //       setTimeout(() => {
+        //         navigate("/login");
+        //         window.location.reload();
+        //       }, 1000);
+        //     }
+        //   );
+        // }
 
         // ...
       })
@@ -324,7 +324,8 @@ export default function Register({ t, languages }) {
             className="register_details"
             component="form"
             noValidate
-            onSubmit={onSignInSubmit}
+            
+            onSubmit={handleSubmit}
             sx={{ mt: 3 }}
           >
             <div id="sign-in-button"></div>
@@ -551,8 +552,9 @@ export default function Register({ t, languages }) {
 
             <Grid  >
               <Button
-                type="submit"
+                type="button"
                 fullWidth
+                onClick={ onSignInSubmit }
                 className="signup-btn"
                 variant="contained"
                 color="success"
@@ -560,6 +562,18 @@ export default function Register({ t, languages }) {
                 sx={{ mt: 3, mb: 2 }}
               >
                 Fetch Otp
+              </Button>
+              <Button
+                type="submit"
+                fullWidth
+                
+                className="signup-btn"
+                variant="contained"
+                color="success"
+                size="large"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                register
               </Button>
             </Grid>
 
